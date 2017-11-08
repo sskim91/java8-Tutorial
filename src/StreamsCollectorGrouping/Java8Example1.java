ebf066791e7e70514477dac538a92b800b0b7ed8
sskim <tjdtjq91@gmail.com>
2017-11-08 17:19:11 +0900

    Java8 Stream Collectors groupingBy examples

---
+++ src/StreamsCollectorGrouping/Java8Example1.java
@@ -0,0 +1,25 @@
+package StreamsCollectorGrouping;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Java8Example1 {
+    public static void main(String[] args) {
+
+        List<String> items = Arrays.asList(
+                "apple", "apple", "banana",
+                "apple", "banana", "oragne", "watermelon"
+        );
+
+        Map<String, Long> result =
+                items.stream().collect(
+                        Collectors.groupingBy(
+                                Function.identity(), Collectors.counting()
+                        ));
+
+        System.out.println(result);
+    }
+}
