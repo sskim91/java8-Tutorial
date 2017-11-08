2cad594e6a46b0902eb41e5ef371a85560ed0bb6
sskim <tjdtjq91@gmail.com>
2017-11-08 18:10:08 +0900

    Java8 Convert Array to Stream

---
+++ src/ConvertArrayToStream/ConvertArrayToStreamInteger.java
@@ -0,0 +1,23 @@
+package ConvertArrayToStream;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class ConvertArrayToStreamInteger {
+    public static void main(String[] args) {
+
+        int[] intArray = {1, 2, 3, 4, 5};
+
+        // 1. Arrays.stream -> IntStream
+        IntStream intStream1 = Arrays.stream(intArray);
+        intStream1.forEach(x -> System.out.println(x));
+
+        // 2. Stream.of -> Stream<int[]>
+        Stream<int[]> temp = Stream.of(intArray);
+
+        // Cant print Stream<int[]> directly, convert / flat it to IntStream
+        IntStream intStream2 = temp.flatMapToInt(x -> Arrays.stream(x));
+        intStream2.forEach(x -> System.out.println(x));
+    }
+}
